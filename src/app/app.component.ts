2d328b7cfa1ba7e5f0e2e224e1887d33af7100d4
pradeep <pradeepkp.ui@gmail.com>
2021-05-14 02:04:49 -0700

    adding todo-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'My Todo-app';
+  todos = [
+    {
+      label: 'Bring Milk',
+      done: false,
+      priority: 3,
+    },
+    {
+      label: 'study',
+      done: true,
+      priority: 1,
+    },
+    {
+      label: 'play music',
+      done: false,
+      priority: 4,
+    },
+    {
+      label: 'play cricket',
+      done: false,
+      priority: 5,
+    },
+  ];
+
+  addTodo(newTodoLabel) {
+    const newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false,
+    };
+
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter((t) => t.label !== todo.label);
+  }
 }
